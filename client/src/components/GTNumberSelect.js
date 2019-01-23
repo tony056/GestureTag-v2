a31c7adf9b60f4725c9f3c31a0ee5e3fb68f094e
Tony Tung <tony61507@gmail.com>
2019-01-23 13:48:38 -0800

    changed form to match new study style

--- client/src/components/GTNumberSelect.js
+++ client/src/components/GTNumberSelect.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Switch, InputNumber } from 'antd';
+import { Switch, InputNumber, Row, Col } from 'antd';
 
 
 export default class GTNumberSelect extends React.Component {
@@ -8,20 +8,54 @@ export default class GTNumberSelect extends React.Component {
     this.state = {
       isFixed: true
     };
-    this.handleChange = this.handleChange.bind(this);
+    this.displayRangeSelect = this.displayRangeSelect.bind(this);
+    this.handleMinChange = this.handleMinChange.bind(this);
+    this.handleMaxChange = this.handleMaxChange.bind(this);
+    this.toggleChange = this.toggleChange.bind(this);
   }
 
-  handleChange(value) {
+  toggleChange(status) {
+    this.setState({ isFixed: status });
+  }
+
+  handleMinChange(value) {
+    const { minChange, name } = this.props;
+    minChange(name, value);
+  }
+
+  handleMaxChange(value) {
+    const { maxChange, name } = this.props;
+    maxChange(name, value);
+  }
+
+  displayRangeSelect() {
+    const { min, max, values, minChange, maxChange } = this.props;
+    return (
+      <div>
+        <Row gutter={8}>
+          <Col span={6}>
+            <b>Min: </b>
+            <InputNumber min={min} max={max} value={values[0]} onChange={this.handleMinChange} />
+          </Col>
+          <Col span={6}>
+            <b>Max: </b>
+            <InputNumber min={min} max={max} value={values[1]} onChange={this.handleMaxChange} />
+          </Col>
+        </Row>
+      </div>
+    );
   }
 
   render() {
     const { isFixed } = this.state;
-    const { min, max } = this.props;
+    const { min, max, values, minChange } = this.props;
     return (
       <div>
-        <Switch checkedChildren="Fixed" unCheckedChildren="Random" checked={isFixed} />
+        <Switch checkedChildren="Fixed" unCheckedChildren="Range" checked={isFixed} onChange={this.toggleChange} />
         <br />
-        {isFixed ? <InputNumber min={min} max={max} defaultValue={min} onChange={this.handleChange} />} 
+        {isFixed ?
+          <InputNumber min={min} max={max} value={values[0]} onChange={this.handleMinChange} />
+        : this.displayRangeSelect()}
       </div>
     );
   }
